026e416e3e37da73e47135fff9e80236d3cebc61
SAE\tshaw3 <tristanjulianshaw@gmail.com>
2017-06-07 14:54:15 +1000

    Some stuff

---
+++ Treacherous Imagery/Assets/EnemyTargetingScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTargetingScript : MonoBehaviour
+{
+
+    public enum Base { Left, Middle, Right };
+    int Role = 5;
+    public Base baseSelected;
+    int xAxis;
+    int yAxis;
+
+    void ChoosingBase()
+    {
+        int role = Random.Range(0, 2);
+        baseSelected = (Base)role;
+    }
+
+    void BaseCoordinates()
+    {
+        //if (baseSelected == Base.Left)
+        //{
+        //    xAxis = A;
+        //    yAxis = B;
+        //}
+        //if (baseSelected == Base.Middle)
+        //{
+        //    xAxis = C;
+        //    yAxis = D;
+        //}
+        //if (baseSelected == Base.Right)
+        //{
+        //    xAxis = E;
+        //    yAxis = F;
+        //}
+        //else
+        //{
+        //    Debug.Log("Error: BaseCoordinates");
+        //}
+    }
+}
---
+++ Treacherous Imagery/Assets/buttonController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class buttonController : MonoBehaviour {
+    public void startGame() {
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("LevelOne");
+    }
+    public void creditsScreen() {
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("Credits");
+    }
+    public void exitGame() {
+        Application.Quit();
+    }
+    public void backButton() {
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("MainMenu");
+    }
+}
