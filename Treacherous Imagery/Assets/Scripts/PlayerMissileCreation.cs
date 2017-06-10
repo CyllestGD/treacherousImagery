1ab3988019d2a4a3391420a99fa8891f1207cd8e
Cyllest <tristanjulianshaw@gmail.com>
2017-06-10 14:45:26 +1000

    Main Menu Completed, Added some scripts, did some prefabs

---
+++ Treacherous Imagery/Assets/Scripts/PlayerMissileCreation.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMissileCreationScript : MonoBehaviour {
+    //public rigidbody PlayerMissile;
+
+	void Update () {
+		
+	}
+}
---
+++ Treacherous Imagery/Assets/Scripts/buttonController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class buttonController : MonoBehaviour {
+    public void startGame() {
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("MainLevel");
+    }
+    public void creditsScreen() {
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("CreditsScreen");
+    }
+    public void exitGame() {
+        Application.Quit();
+    }
+    public void backButton() {
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("MainMenu");
+    }
+}
