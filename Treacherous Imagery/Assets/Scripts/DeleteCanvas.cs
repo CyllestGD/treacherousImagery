db84df12d47cc632aaa4a2cc01ad8a94f76a91fd
Cyllest <tristanjulianshaw@gmail.com>
2017-06-20 14:25:09 +1000

    Bunch of new scenes and some new assets to go with them - still a lot to update

---
+++ Treacherous Imagery/Assets/Scripts/DeleteCanvas.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteCanvas : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Delete"))
+        {
+            Destroy(transform.parent.gameObject);
+        }
+    }
+}
--- Treacherous Imagery/Assets/Scripts/buttonController.cs
+++ Treacherous Imagery/Assets/Scripts/buttonController.cs
@@ -4,17 +4,59 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class buttonController : MonoBehaviour {
-    public void startGame() {
-        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("MainLevel");
+public class buttonController : MonoBehaviour
+{
+    //public int index;
+    //public string sceneName;
+    //public Image black;
+    //public Animator anim;
+
+    public void playButton()
+    {
+        //StartCoroutine(Fading());
+        SceneManager.LoadSceneAsync("Instructions");
     }
-    public void creditsScreen() {
-        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("CreditsScreen");
+    public void startGame()
+    {
+        SceneManager.LoadSceneAsync("MainLevel");
     }
-    public void exitGame() {
+    public void aboutButton()
+    {
+        //StartCoroutine(Fading());
+        SceneManager.LoadSceneAsync("About");
+    }
+    public void creditsScreen()
+    {
+        SceneManager.LoadSceneAsync("Credits1");
+    }
+    public void credits1Forward()
+    {
+        SceneManager.LoadSceneAsync("Credits2");
+    }
+    public void credits2Back()
+    {
+        SceneManager.LoadSceneAsync("Credits1");
+    }
+    public void credits2Forward()
+    {
+        SceneManager.LoadSceneAsync("Credits3");
+    }
+    public void credits3Back()
+    {
+        SceneManager.LoadSceneAsync("Credits2");
+    }
+    public void exitGame()
+    {
         Application.Quit();
     }
-    public void backButton() {
-        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("MainMenu");
+    public void menuButton()
+    {
+        SceneManager.LoadSceneAsync("MainMenu");
     }
+    //IEnumerator Fading()
+    //{
+    //    anim.SetBool("Fade", true);
+    //    yield return new WaitUntil(() => black.color.a == 1);
+    //    //SceneManager.LoadScene(index);
+    //}
 }
